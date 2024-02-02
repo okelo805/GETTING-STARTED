5ce7095769a9193a201ce6875cbeff4929227c48
okelo805 <158202554+okelo805@users.noreply.github.com>
2024-02-02 18:07:44 +0300

    Add files via upload

---
+++ hello world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("my name is brian\n");
+    printf("my course is civil engineering\n");
+    printf("my favourite team is inter milan\n");
+    printf("my favourite meal is chapati and beans\n");
+    return 0;
+}
